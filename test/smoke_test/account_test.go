1322a566b9df3b511d0c11364d098a4a535587a3
G-Despacito <zhangxinyi2105@gmail.com>
2024-07-22 11:51:08 -0700

    fix test

--- test/smoke_test/account_test.go
+++ test/smoke_test/account_test.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/omnistrate/ctl/cmd"
-	createaccount "github.com/omnistrate/ctl/cmd/create/account"
-	deleteaccount "github.com/omnistrate/ctl/cmd/deletec/account"
-	describeaccount "github.com/omnistrate/ctl/cmd/describe/account"
-	getaccount "github.com/omnistrate/ctl/cmd/get/account"
 	"github.com/omnistrate/ctl/test/testutils"
 	"testing"
 
@@ -33,32 +29,35 @@ func Test_account_basic(t *testing.T) {
 	gcpAccountName := "gcp" + uuid.NewString()
 
 	// PASS: create aws account
-	createaccount.AccountCmd.SetArgs([]string{"account", awsAccountName, "--aws-account-id", "123456789012", "--aws-bootstrap-role-arn", "arn:aws:iam::123456789012:role/role-name"})
-	err = createaccount.AccountCmd.Execute()
-	require.NoError(err)
+	cmd.RootCmd.SetArgs([]string{"create", "account", awsAccountName, "--aws-account-id", "123456789012"})
+	err = cmd.RootCmd.Execute()
+	require.Error(err)
+	require.Contains(err.Error(), "unauthorized: only root users can onboard accounts")
 
 	// PASS: create gcp account
-	createaccount.AccountCmd.SetArgs([]string{"account", gcpAccountName, "--gcp-project-id", "project-id", "--gcp-project-number", "project-number", "--gcp-service-account-email", "service-account-email"})
-	err = createaccount.AccountCmd.Execute()
-	require.NoError(err)
+	cmd.RootCmd.SetArgs([]string{"create", "account", gcpAccountName, "--gcp-project-id", "project-id", "--gcp-project-number", "project-number"})
+	err = cmd.RootCmd.Execute()
+	require.Error(err)
+	require.Contains(err.Error(), "unauthorized: only root users can onboard accounts")
 
 	// PASS: get accounts
-	getaccount.AccountCmd.SetArgs([]string{"account"})
-	err = getaccount.AccountCmd.Execute()
+	cmd.RootCmd.SetArgs([]string{"get", "account"})
+	err = cmd.RootCmd.Execute()
 	require.NoError(err)
 
 	// PASS: get accounts by name
-	getaccount.AccountCmd.SetArgs([]string{"account", awsAccountName, gcpAccountName})
-	err = getaccount.AccountCmd.Execute()
+	cmd.RootCmd.SetArgs([]string{"get", "account", awsAccountName, gcpAccountName})
+	err = cmd.RootCmd.Execute()
 	require.NoError(err)
 
 	// PASS: describe accounts
-	describeaccount.AccountCmd.SetArgs([]string{"account", awsAccountName, gcpAccountName})
-	err = describeaccount.AccountCmd.Execute()
+	cmd.RootCmd.SetArgs([]string{"describe", "account", awsAccountName, gcpAccountName})
+	err = cmd.RootCmd.Execute()
 	require.NoError(err)
 
 	// PASS: delete accounts
-	deleteaccount.AccountCmd.SetArgs([]string{"account", awsAccountName, gcpAccountName})
-	err = deleteaccount.AccountCmd.Execute()
-	require.NoError(err)
+	cmd.RootCmd.SetArgs([]string{"delete", "account", awsAccountName, gcpAccountName})
+	err = cmd.RootCmd.Execute()
+	require.Error(err)
+	require.Contains(err.Error(), "account(s) not found")
 }
