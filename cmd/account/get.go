82d3ae3bb33f6a7c6baf56b5fa0efa602f86c465
G-Despacito <zhangxinyi2105@gmail.com>
2024-08-13 18:35:09 -0700

    Draft

--- cmd/account/get.go
+++ cmd/account/get.go
@@ -1,1 +1,140 @@
 package account
+
+import (
+	"fmt"
+	accountconfigapi "github.com/omnistrate/api-design/v1/pkg/registration/gen/account_config_api"
+	"github.com/omnistrate/ctl/dataaccess"
+	"github.com/omnistrate/ctl/utils"
+	"github.com/spf13/cobra"
+	"os"
+	"text/tabwriter"
+)
+
+var (
+	accountExample = `  # Get all accounts
+  omnistrate-ctl get account
+
+  # Get account with name
+  omnistrate-ctl get account <name>
+
+  # Get multiple accounts
+  omnistrate-ctl get account <name1> <name2> <name3>
+
+  # Get account with ID
+  omnistrate-ctl get account <id> --id
+
+  # Get multiple accounts with IDs
+  omnistrate-ctl get account <id1> <id2> <id3> --id`
+)
+
+// AccountCmd represents the describe command
+var AccountCmd = &cobra.Command{
+	Use:     "account <name>",
+	Short:   "Display one or more accounts",
+	Long:    `The get account command displays basic information about one or more accounts.`,
+	Example: accountExample,
+	RunE:    Run,
+	PostRun: func(cmd *cobra.Command, args []string) {
+		dataaccess.AskVerifyAccountIfAny()
+	},
+	SilenceUsage: true,
+}
+
+func init() {
+	AccountCmd.Flags().Bool("id", false, "Specify account ID instead of name")
+}
+
+func Run(cmd *cobra.Command, args []string) error {
+	token, err := utils.GetToken()
+	if err != nil {
+		utils.PrintError(err)
+		return err
+	}
+
+	var ID bool
+	ID, err = cmd.Flags().GetBool("id")
+	if err != nil {
+		utils.PrintError(err)
+		return err
+	}
+
+	var accounts []*accountconfigapi.DescribeAccountConfigResult
+	if ID {
+		for _, id := range args {
+			var account *accountconfigapi.DescribeAccountConfigResult
+			account, err = dataaccess.DescribeAccount(id, token)
+			if err != nil {
+				utils.PrintError(err)
+				return err
+			}
+			accounts = append(accounts, account)
+		}
+	} else {
+		// List all accounts
+		var listRes *accountconfigapi.ListAccountConfigResult
+		listRes, err = dataaccess.ListAccounts(token, "all")
+		if err != nil {
+			utils.PrintError(err)
+			return err
+		}
+		allAccounts := listRes.AccountConfigs
+
+		// Print accounts table if no account name is provided
+		if len(args) == 0 {
+			utils.PrintSuccess(fmt.Sprintf("%d account(s) found", len(allAccounts)))
+			if len(allAccounts) > 0 {
+				printTable(allAccounts)
+			}
+			return nil
+		}
+
+		// Format listRes.Accounts into a map
+		accountMap := make(map[string]*accountconfigapi.DescribeAccountConfigResult)
+		for _, account := range allAccounts {
+			accountMap[account.Name] = account
+		}
+
+		// Filter accounts by name
+		for _, name := range args {
+			account, ok := accountMap[name]
+			if !ok {
+				utils.PrintError(fmt.Errorf("account '%s' not found", name))
+				continue
+			}
+			accounts = append(accounts, account)
+		}
+	}
+
+	printTable(accounts)
+
+	return nil
+}
+
+func printTable(accounts []*accountconfigapi.DescribeAccountConfigResult) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
+
+	fmt.Fprintln(w, "ID\tName\tStatus\tCloud Provider\tTarget Account ID")
+
+	for _, account := range accounts {
+		var targetAccountID, cloudProvider string
+		if account.AwsAccountID != nil {
+			targetAccountID = *account.AwsAccountID
+			cloudProvider = "AWS"
+		} else {
+			targetAccountID = fmt.Sprintf("%s(ProjectID: %s)", *account.GcpProjectID, *account.GcpProjectNumber)
+			cloudProvider = "GCP"
+		}
+
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n",
+			account.ID,
+			account.Name,
+			account.Status,
+			cloudProvider,
+			targetAccountID)
+	}
+
+	err := w.Flush()
+	if err != nil {
+		utils.PrintError(err)
+	}
+}
--- cmd/service/describe.go
+++ cmd/service/describe.go
@@ -1,1 +1,118 @@
 package service
+
+import (
+	"encoding/json"
+	"fmt"
+	serviceapi "github.com/omnistrate/api-design/v1/pkg/registration/gen/service_api"
+	"github.com/omnistrate/ctl/dataaccess"
+	"github.com/omnistrate/ctl/utils"
+	"github.com/spf13/cobra"
+	"slices"
+	"strings"
+)
+
+const (
+	serviceExample = `  # Describe service with name
+  omnistrate-ctl describe service <name>
+
+  # Describe service with ID
+  omnistrate-ctl describe service <id> --id
+
+  # Describe multiple services with names
+  omnistrate-ctl describe service <name1> <name2> <name3>
+
+  # Describe multiple services with IDs
+  omnistrate-ctl describe service <id1> <id2> <id3> --id`
+)
+
+var ServiceCmd = &cobra.Command{
+	Use:          "service <name>",
+	Short:        "Display details for one or more services",
+	Long:         "Display detailed information about the service by specifying the service name or ID.",
+	Example:      serviceExample,
+	RunE:         Run,
+	SilenceUsage: true,
+}
+
+func init() {
+	ServiceCmd.Args = cobra.MinimumNArgs(1) // Require at least one argument
+
+	ServiceCmd.Flags().Bool("id", false, "Specify service ID instead of name")
+}
+
+func Run(cmd *cobra.Command, args []string) error {
+	token, err := utils.GetToken()
+	if err != nil {
+		utils.PrintError(err)
+		return err
+	}
+
+	var ID bool
+	ID, err = cmd.Flags().GetBool("id")
+	if err != nil {
+		utils.PrintError(err)
+		return err
+	}
+
+	var services []*serviceapi.DescribeServiceResult
+	if ID {
+		for _, name := range args {
+			service, err := dataaccess.DescribeService(name, token)
+			if err != nil {
+				utils.PrintError(err)
+				return err
+			}
+			services = append(services, service)
+		}
+	} else {
+		// List services
+		listRes, err := dataaccess.ListServices(token)
+		if err != nil {
+			utils.PrintError(err)
+			return err
+		}
+
+		found := make(map[string]bool)
+		for _, name := range args {
+			found[name] = false
+		}
+
+		// Filter services by name
+		for _, s := range listRes.Services {
+			if slices.Contains(args, s.Name) {
+				service, err := dataaccess.DescribeService(string(s.ID), token)
+				if err != nil {
+					utils.PrintError(err)
+					return err
+				}
+				services = append(services, service)
+				found[s.Name] = true
+			}
+		}
+
+		namesNotFound := make([]string, 0)
+		for name, ok := range found {
+			if !ok {
+				namesNotFound = append(namesNotFound, name)
+			}
+		}
+
+		if len(namesNotFound) > 0 {
+			err = fmt.Errorf("service(s) not found: %s", strings.Join(namesNotFound, ", "))
+			utils.PrintError(err)
+			return err
+		}
+	}
+
+	// Print service details
+	for _, service := range services {
+		data, err := json.MarshalIndent(service, "", "    ")
+		if err != nil {
+			utils.PrintError(err)
+			return err
+		}
+		fmt.Println(string(data))
+	}
+
+	return nil
+}
