db344f622c6b7bb7065559ae38c68281a7a750a3
G-Despacito <zhangxinyi2105@gmail.com>
2024-08-21 16:55:45 -0700

    Update docs

--- cmd/build/build.go
+++ cmd/build/build.go
@@ -51,22 +51,28 @@ const (
 	DockerComposeSpecType = "DockerCompose"
 	ServicePlanSpecType   = "ServicePlanSpec"
 
-	buildExample = `  # Build in dev environment
+	buildExample = `  # Build service with image in dev environment
+  omnistrate-ctl build --image docker.io/mysql:5.7 --name MySQL --env-var MYSQL_ROOT_PASSWORD:password --env-var MYSQL_DATABASE:mydb
+
+  # Build service with private image in dev environment
+  omnistrate-ctl build --image docker.io/namespace/my-image:v1.2 --name "My Service" --image-registry-auth-username username --image-registry-auth-password password --env-var KEY1:VALUE1 --env-var KEY2:VALUE2
+
+  # Build service with compose spec in dev environment
   omnistrate-ctl build --file docker-compose.yml --name "My Service"
 
-  # Build in prod environment
+  # Build service with compose spec in prod environment
   omnistrate-ctl build --file docker-compose.yml --name "My Service" --environment prod --environment-type prod
 
-  # Build and release the service with a specific release version name
+  # Build service with compose spec and release the service with a specific release version name
   omnistrate-ctl build --file docker-compose.yml --name "My Service" --release --release-name "v1.0.0-alpha"
 
-  # Build and release the service as preferred with a specific release version name
+  # Build service with compose spec and release the service as preferred with a specific release version name
   omnistrate-ctl build --file docker-compose.yml --name "My Service" --release-as-preferred --release-name "v1.0.0-alpha"
 
-  # Build interactively
+  # Build service with compose spec interactively
   omnistrate-ctl build --file docker-compose.yml --name "My Service" --interactive
 
-  # Build with service description and service logo
+  # Build service with compose spec with service description and service logo
   omnistrate-ctl build --file docker-compose.yml --name "My Service" --description "My Service Description" --service-logo-url "https://example.com/logo.png"
 `
 
