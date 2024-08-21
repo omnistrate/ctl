7c8d86cf638568339d94ffaa8eb249f0b8f0026b
G-Despacito <zhangxinyi2105@gmail.com>
2024-08-21 10:27:05 -0700

    Minor

--- model/environment.go
+++ model/environment.go
@@ -10,15 +10,15 @@ type Environment struct {
 }
 
 type DetailedEnvironment struct {
-	EnvironmentID    string `json:"environment_id"`
-	EnvironmentName  string `json:"environment_name"`
-	EnvironmentType  string `json:"environment_type"`
-	ServiceID        string `json:"service_id"`
-	ServiceName      string `json:"service_name"`
-	SourceEnvName    string `json:"source_env_name"`
-	PromoteStatus    string `json:"promote_status"`
-	SaaSPortalStatus string `json:"saas_portal_status"`
-	SaaSPortalURL    string `json:"saas_portal_url"`
+	EnvironmentID    string `json:"environment_id,omitempty"`
+	EnvironmentName  string `json:"environment_name,omitempty"`
+	EnvironmentType  string `json:"environment_type,omitempty"`
+	ServiceID        string `json:"service_id,omitempty"`
+	ServiceName      string `json:"service_name,omitempty"`
+	SourceEnvName    string `json:"source_env_name,omitempty"`
+	PromoteStatus    string `json:"promote_status,omitempty"`
+	SaaSPortalStatus string `json:"saas_portal_status,omitempty"`
+	SaaSPortalURL    string `json:"saas_portal_url,omitempty"`
 }
 
 type Promotion struct {
