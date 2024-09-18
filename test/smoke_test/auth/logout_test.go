affec5c3701e2d66f7830c7b645f9c03616ed813
G-Despacito <zhangxinyi2105@gmail.com>
2024-09-18 16:51:08 -0700

    Fix issue

--- test/smoke_test/auth/logout_test.go
+++ test/smoke_test/auth/logout_test.go
@@ -24,7 +24,7 @@ func Test_logout(t *testing.T) {
 	cmd.RootCmd.SetArgs([]string{"logout"})
 	err = cmd.RootCmd.Execute()
 	require.Error(err)
-	require.Contains(err.Error(), config.ErrConfigFileNotFound.Error())
+	require.Contains(err.Error(), "config file not found")
 
 	// PASS: logout after login
 	testEmail, testPassword, err := testutils.GetSmokeTestAccount()
