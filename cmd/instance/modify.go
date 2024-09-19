4f706f3f9145f9e8ca73384564fe69b5500175e0
G-Despacito <zhangxinyi2105@gmail.com>
2024-09-18 17:05:29 -0700

    Gen doc

--- cmd/instance/modify.go
+++ cmd/instance/modify.go
@@ -12,7 +12,10 @@ import (
 
 const (
 	modifyExample = `# Modify an instance deployment
-omctl instance modify instance-abcd1234 --param '{"databaseName":"default","password":"a_secure_password","rootPassword":"a_secure_root_password","username":"user"}'`
+omctl instance modify instance-abcd1234 --param '{"databaseName":"default","password":"a_secure_password","rootPassword":"a_secure_root_password","username":"user"}'
+
+# Modify an instance deployment using a parameter file
+omctl instance modify instance-abcd1234 --param-file /path/to/param.json`
 )
 
 var modifyCmd = &cobra.Command{
