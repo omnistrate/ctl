2faa0d70b3fa2a70968283fbd35a26da953005c7
pberton <1789738+pberton@users.noreply.github.com>
2024-10-01 16:29:25 -0700

    remove unused code

--- utils/env_test.go
+++ utils/env_test.go
@@ -18,22 +18,6 @@ func TestGetEnvDefault(t *testing.T) {
 	assert.Equal(t, "default", value)
 }
 
-func TestGetEnvAsInteger(t *testing.T) {
-	os.Setenv("test_key", "1")
-	value := GetEnvAsInteger("test_key", "default")
-	assert.Equal(t, 1, value)
-}
-
-func TestGetEnvAsIntegerDefault(t *testing.T) {
-	value := GetEnvAsInteger("inexistent", "1")
-	assert.Equal(t, 1, value)
-}
-
-func TestGetEnvAsIntegerDefaultNaN(t *testing.T) {
-	value := GetEnvAsInteger("inexistent", "default")
-	assert.Equal(t, 0, value)
-}
-
 func TestGetEnvAsBooleanTrue(t *testing.T) {
 	os.Setenv("test_key", "true")
 	value := GetEnvAsBoolean("test_key", "false")
@@ -56,26 +40,4 @@ func TestGetEnvAsBooleanInvalid(t *testing.T) {
 	os.Setenv("test_key", "1")
 	value := GetEnvAsBoolean("test_key", "true")
 	assert.False(t, value)
-}
-
-func TestGetEnvAsInt64(t *testing.T) {
-	os.Setenv("test_key", "1")
-	value := GetEnvAsInt64("test_key", "default")
-	assert.Equal(t, int64(1), value)
-}
-
-func TestGetEnvAsInt64Default(t *testing.T) {
-	value := GetEnvAsInt64("inexistent", "1")
-	assert.Equal(t, int64(1), value)
-}
-
-func TestGetEnvAsFloat64(t *testing.T) {
-	os.Setenv("test_key", "1.1")
-	value := GetEnvAsFloat64("test_key", "default")
-	assert.Equal(t, float64(1.1), value)
-}
-
-func TestGetEnvAsFloat4Default(t *testing.T) {
-	value := GetEnvAsFloat64("inexistent", "1.1")
-	assert.Equal(t, float64(1.1), value)
 }
