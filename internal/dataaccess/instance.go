0afd8973c19a72864803cbd8444a9a5c087ca289
G-Despacito <zhangxinyi2105@gmail.com>
2024-11-29 10:29:19 -0800

    Use open api client for describe instance data access layer method

--- internal/dataaccess/instance.go
+++ internal/dataaccess/instance.go
@@ -2,30 +2,31 @@ package dataaccess
 
 import (
 	"context"
-
 	"github.com/omnistrate/api-design/pkg/httpclientwrapper"
 	inventoryapi "github.com/omnistrate/api-design/v1/pkg/fleet/gen/inventory_api"
 	"github.com/omnistrate/ctl/internal/config"
+	openapiclientfleet "github.com/omnistrate/omnistrate-sdk-go/fleet"
 )
 
-func DescribeInstance(ctx context.Context, token, serviceID, serviceEnvironmentID, instanceID string) (instance *inventoryapi.ResourceInstance, err error) {
-	fleetService, err := httpclientwrapper.NewInventory(config.GetHostScheme(), config.GetHost())
+func DescribeInstance(ctx context.Context, token, serviceID, serviceEnvironmentID, instanceID string) (instance *openapiclientfleet.ResourceInstance, err error) {
+	ctxWithToken := context.WithValue(ctx, openapiclientfleet.ContextAccessToken, token)
+
+	configuration := openapiclientfleet.NewConfiguration()
+	apiClient := openapiclientfleet.NewAPIClient(configuration)
+	res, r, err := apiClient.InventoryApiAPI.InventoryApiDescribeResourceInstance(
+		ctxWithToken,
+		serviceID,
+		serviceEnvironmentID,
+		instanceID,
+	).Execute()
+
+	err = handleFleetError(err)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	request := &inventoryapi.DescribeResourceInstanceRequestInternal{
-		Token:         token,
-		ServiceID:     inventoryapi.ServiceID(serviceID),
-		EnvironmentID: inventoryapi.ServiceEnvironmentID(serviceEnvironmentID),
-		InstanceID:    inventoryapi.ResourceInstanceID(instanceID),
-	}
-
-	if instance, err = fleetService.DescribeResourceInstance(ctx, request); err != nil {
-		return
-	}
-
-	return
+	r.Body.Close()
+	return res, nil
 }
 
 func CreateInstance(ctx context.Context, token string, request inventoryapi.FleetCreateResourceInstanceRequest) (res *inventoryapi.CreateResourceInstanceResult, err error) {
