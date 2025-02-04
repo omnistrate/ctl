b7e17b8759200ae95292ba9672f03a54cd4458e0
G-Despacito <zhangxinyi2105@gmail.com>
2025-02-04 11:09:15 -0800

    Support upgrade instances with version name

--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -23,7 +23,10 @@ omctl upgrade [instance1] [instance2] --version=2.0
 omctl upgrade [instance1] [instance2] --version=latest
 
  # Upgrade instances to the preferred version
-omctl upgrade [instance1] [instance2] --version=preferred`
+omctl upgrade [instance1] [instance2] --version=preferred
+
+# Upgrade instances to a specific version with version name
+omctl upgrade [instance1] [instance2] --version-name=v0.1.1`
 )
 
 var Cmd = &cobra.Command{
@@ -41,11 +44,7 @@ func init() {
 	Cmd.Args = cobra.MinimumNArgs(1)
 
 	Cmd.Flags().StringP("version", "", "", "Specify the version number to upgrade to. Use 'latest' to upgrade to the latest version. Use 'preferred' to upgrade to the preferred version.")
-
-	err := Cmd.MarkFlagRequired("version")
-	if err != nil {
-		return
-	}
+	Cmd.Flags().StringP("version-name", "", "", "Specify the version name to upgrade to. Use either this flag or the version flag to upgrade to a specific version.")
 }
 
 type Args struct {
@@ -72,8 +71,27 @@ func run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	versionName, err := cmd.Flags().GetString("version-name")
+	if err != nil {
+		utils.PrintError(err)
+		return err
+	}
+
 	output, err := cmd.Flags().GetString("output")
 	if err != nil {
+		utils.PrintError(err)
+		return err
+	}
+
+	// Validate input arguments
+	if version == "" && versionName == "" {
+		err = errors.New("version or version name is required")
+		utils.PrintError(err)
+		return err
+	}
+
+	if version != "" && versionName != "" {
+		err = errors.New("please provide either version or version name, not both")
 		utils.PrintError(err)
 		return err
 	}
@@ -139,21 +157,48 @@ func run(cmd *cobra.Command, args []string) error {
 		sourceVersion = describeRes.TierVersion
 
 		// Get the target version
-		switch version {
-		case "latest":
-			targetVersion, err = dataaccess.FindLatestVersion(cmd.Context(), token, serviceID, productTierID)
+		if version != "" {
+			switch version {
+			case "latest":
+				targetVersion, err = dataaccess.FindLatestVersion(cmd.Context(), token, serviceID, productTierID)
+				if err != nil {
+					utils.HandleSpinnerError(spinner, sm, err)
+					return err
+				}
+			case "preferred":
+				targetVersion, err = dataaccess.FindPreferredVersion(cmd.Context(), token, serviceID, productTierID)
+				if err != nil {
+					utils.HandleSpinnerError(spinner, sm, err)
+					return err
+				}
+			default:
+				targetVersion = version
+			}
+		} else {
+			allVersions, err := dataaccess.ListVersions(cmd.Context(), token, serviceID, productTierID)
 			if err != nil {
 				utils.HandleSpinnerError(spinner, sm, err)
 				return err
 			}
-		case "preferred":
-			targetVersion, err = dataaccess.FindPreferredVersion(cmd.Context(), token, serviceID, productTierID)
-			if err != nil {
-				utils.HandleSpinnerError(spinner, sm, err)
-				return err
+
+			targetVersions := make([]string, 0)
+			for _, versionSet := range allVersions.TierVersionSets {
+				if versionSet.Name != nil && *versionSet.Name == versionName {
+					targetVersions = append(targetVersions, versionSet.Version)
+				}
 			}
-		default:
-			targetVersion = version
+
+			if len(targetVersions) == 0 {
+				err = fmt.Errorf("version name %s not found", versionName)
+				utils.HandleSpinnerError(spinner, sm, err)
+			}
+
+			if len(targetVersions) > 1 {
+				err = fmt.Errorf("multiple versions found for version name %s", versionName)
+				utils.HandleSpinnerError(spinner, sm, err)
+			}
+
+			targetVersion = targetVersions[0]
 		}
 
 		// Check if the target version exists
