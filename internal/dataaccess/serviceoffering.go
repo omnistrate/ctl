128f8eaba5d4bb4c02093227c3d119621e432ad6
pberton <1789738+pberton@users.noreply.github.com>
2025-02-17 23:11:33 -0800

    moving to new sdk

--- internal/dataaccess/serviceoffering.go
+++ internal/dataaccess/serviceoffering.go
@@ -2,64 +2,72 @@ package dataaccess
 
 import (
 	"context"
+	"net/http"
 
-	"github.com/omnistrate/api-design/pkg/httpclientwrapper"
-	inventoryapi "github.com/omnistrate/api-design/v1/pkg/fleet/gen/inventory_api"
-	"github.com/omnistrate/ctl/internal/config"
-	"github.com/omnistrate/ctl/internal/utils"
+	openapiclientfleet "github.com/omnistrate-oss/omnistrate-sdk-go/fleet"
 )
 
-func ListServiceOfferings(ctx context.Context, token, orgID string) (*inventoryapi.InventoryListServiceOfferingsResult, error) {
-	inventory, err := httpclientwrapper.NewInventory(config.GetHostScheme(), config.GetHost())
+func ListServiceOfferings(ctx context.Context, token, orgID string) (inventory *openapiclientfleet.InventoryListServiceOfferingsResult, err error) {
+	ctxWithToken := context.WithValue(ctx, openapiclientfleet.ContextAccessToken, token)
+	apiClient := getFleetClient()
+
+	req := apiClient.InventoryApiAPI.InventoryApiListServiceOfferings(ctxWithToken)
+	req.OrgId(orgID)
+
+	var r *http.Response
+	defer func() {
+		if r != nil {
+			_ = r.Body.Close()
+		}
+	}()
+
+	inventory, r, err = req.Execute()
 	if err != nil {
-		return nil, err
+		return nil, handleFleetError(err)
 	}
 
-	res, err := inventory.ListServiceOffering(ctx, &inventoryapi.InventoryListServiceOfferingsRequest{
-		Token: token,
-	})
+	return inventory, nil
+}
+
+func DescribeServiceOfferingResource(ctx context.Context, token, serviceID, resourceID, instanceID, productTierID, productTierVersion string) (res *openapiclientfleet.InventoryDescribeServiceOfferingResourceResult, err error) {
+	ctxWithToken := context.WithValue(ctx, openapiclientfleet.ContextAccessToken, token)
+	apiClient := getFleetClient()
+
+	req := apiClient.InventoryApiAPI.InventoryApiDescribeServiceOfferingResource(ctxWithToken, serviceID, resourceID, instanceID)
+	req.ProductTierId(productTierID)
+	req.ProductTierVersion(productTierVersion)
+	var r *http.Response
+	defer func() {
+		if r != nil {
+			_ = r.Body.Close()
+		}
+	}()
+
+	res, r, err = req.Execute()
 	if err != nil {
-		return nil, err
+		return nil, handleFleetError(err)
 	}
 
 	return res, nil
 }
 
-func DescribeServiceOfferingResource(ctx context.Context, token, serviceID, resourceID, instanceID, productTierID, productTierVersion string) (*inventoryapi.InventoryDescribeServiceOfferingResourceResult, error) {
-	inventory, err := httpclientwrapper.NewInventory(config.GetHostScheme(), config.GetHost())
-	if err != nil {
-		return nil, err
-	}
+func DescribeServiceOffering(ctx context.Context, token, serviceID, productTierID, productTierVersion string) (res *openapiclientfleet.InventoryDescribeServiceOfferingResult, err error) {
+	ctxWithToken := context.WithValue(ctx, openapiclientfleet.ContextAccessToken, token)
+	apiClient := getFleetClient()
 
-	res, err := inventory.DescribeServiceOfferingResource(ctx, &inventoryapi.InventoryDescribeServiceOfferingResourceRequest{
-		Token:              token,
-		ServiceID:          inventoryapi.ServiceID(serviceID),
-		ResourceID:         inventoryapi.ResourceID(resourceID),
-		InstanceID:         instanceID,
-		ProductTierID:      (*inventoryapi.ProductTierID)(utils.ToPtr(productTierID)),
-		ProductTierVersion: utils.ToPtr(productTierVersion),
-	})
-	if err != nil {
-		return nil, err
-	}
+	req := apiClient.InventoryApiAPI.InventoryApiDescribeServiceOffering(ctxWithToken, serviceID)
+	req.ProductTierId(productTierID)
+	req.ProductTierVersion(productTierVersion)
+	var r *http.Response
+	defer func() {
+		if r != nil {
+			_ = r.Body.Close()
+		}
+	}()
 
-	return res, nil
-}
-
-func DescribeServiceOffering(ctx context.Context, token, serviceID, productTierID, productTierVersion string) (*inventoryapi.InventoryDescribeServiceOfferingResult, error) {
-	inventory, err := httpclientwrapper.NewInventory(config.GetHostScheme(), config.GetHost())
+	res, r, err = req.Execute()
 	if err != nil {
-		return nil, err
-	}
-
-	res, err := inventory.DescribeServiceOffering(ctx, &inventoryapi.InventoryDescribeServiceOfferingRequest{
-		Token:              token,
-		ServiceID:          inventoryapi.ServiceID(serviceID),
-		ProductTierID:      (*inventoryapi.ProductTierID)(utils.ToPtr(productTierID)),
-		ProductTierVersion: utils.ToPtr(productTierVersion),
-	})
-	if err != nil {
-		return nil, err
+		return nil, handleFleetError(err)
 	}
 
 	return res, nil
